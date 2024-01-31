531f4aec94f575ac530049a6bdcc017d149cba37
Risav03 <needleweed03@gmail.com>
2024-01-31 19:35:41 +0530

    minimart

--- tacoMinimart.sol
+++ tacoMinimart.sol
@@ -71,12 +71,20 @@ abstract contract Ownable is Context {
     }
 }
 
+struct returnData {
+    address contractAddress;
+    uint256 tokenId;
+    address owner;
+    uint256 itemPrice;
+} 
+
 interface IGUAC{
     function transferFrom(address from, address to, uint256 amount) external;
 }
 
 interface INft{
-    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+   function safeTransferFrom(address from, address to, uint256 tokenId) external;
+    function ownerOf(uint256 tokenId) external view returns(address);
 }
 
 abstract contract MiniMart is Ownable{
@@ -84,60 +92,101 @@ abstract contract MiniMart is Ownable{
     IGUAC public guacToken;
     INft public nftToTransfer;
 
-    mapping(uint => address) minimartItemContract;
-    mapping(uint => uint256) minimartItemTokenId;
+    mapping(uint => address) public minimartItemContract;
+    mapping(uint => uint256) public minimartItemTokenId;
 
-    mapping(uint => address) indexOwner;
-    mapping(uint => address) approvedCollection;
+    mapping(uint => address) public approvedCollection;
 
-    mapping(uint => uint256) minimartItemPrice;
+    mapping(uint => uint256) public minimartItemPrice;
 
-    uint256 minimartIndex;
+
+    uint256 public num = 1;
+    uint256 public totalListed;
+
 
      constructor(){
         guacToken = IGUAC(0x6FE947Ffd91aE3A7C8a6090B692cA2BDeCD30269);
     }
 
-    function setApprovedCollection(address collection, uint num) public onlyOwner{
-        approvedCollection[num] = collection;
+    function fetchData() public view returns (returnData[] memory){
+
+        returnData[] memory dataArr = new returnData[](totalListed);
+
+        for(uint256 index = 0; index<totalListed; index++){
+
+            if(minimartItemContract[index] != address(0)){
+                dataArr[index] = returnData(minimartItemContract[index], minimartItemTokenId[index], INft(minimartItemContract[index]).ownerOf(minimartItemTokenId[index]), minimartItemPrice[index]);
+            }
+        }
+        return dataArr;
     }
 
-    function removeApprovedCollection(uint num) public onlyOwner{
-        delete approvedCollection[num];
+    function returnApprovedContracts() public view returns(address[] memory) {
+        address[] memory addArr = new address[](num);
+        for(uint i = 0; i <num-1; i++){
+            addArr[i] = approvedCollection[i];
+        }
+        return addArr;
     }
 
-    function setMinimartItem(address contractAddress, uint256 tokenId, uint256 price) public {
-        minimartItemContract[minimartIndex] = contractAddress;
-        minimartItemTokenId[minimartIndex] = tokenId;
-        minimartItemPrice[minimartIndex] = price;
-
-        indexOwner[minimartIndex] = msg.sender;
-
-        while(minimartItemContract[minimartIndex] != address(0)){
-            minimartIndex++;
+    function setApprovedCollection(address collection) public onlyOwner{
+        for(uint256 i = 0; i<=num; i++){
+            if(approvedCollection[i] == address(0)){
+               approvedCollection[i] = collection;
+                num++;   
+               break;
+            }
         }
     }
 
-    function buyMinimartItem(uint256 cost, uint256 index) public{
-        require(cost == minimartItemPrice[index], "Low amount");
-        guacToken.transferFrom(msg.sender, indexOwner[index], minimartItemPrice[index]);
-
-        nftToTransfer = INft(minimartItemContract[index]);
-        nftToTransfer.safeTransferFrom(indexOwner[index], msg.sender, minimartItemTokenId[index]);
-
-        delete minimartItemContract[index];
-        delete minimartItemTokenId[index];
-        delete minimartItemPrice[index];
-
-        minimartIndex = index;
+    function removeApprovedCollection(uint index) public onlyOwner{
+        delete approvedCollection[index];
     }
 
-    function unListItem(uint256 index) public{
-        require(msg.sender == indexOwner[index], "Not the Owner");
+    function setMinimartItem(address contractAddress, uint256 tokenId, uint256 price) public {
+        bool inside = false;
+
+        for(uint256 i = 0; i<totalListed; i++){
+            if(minimartItemContract[i] == address(0)){
+                minimartItemContract[i] = contractAddress;
+                minimartItemTokenId[i] = tokenId;
+                minimartItemPrice[i] = price;
+                inside = true;
+                break;
+            }
+            
+        }
+
+        if(!inside){
+            minimartItemContract[totalListed] = contractAddress;
+            minimartItemTokenId[totalListed] = tokenId;
+            minimartItemPrice[totalListed] = price;
+            totalListed++;
+        }
+
+    }
+
+    function buyMinimartItem(uint256 cost, uint256 index) public{
+        nftToTransfer = INft(minimartItemContract[index]);
+        require(cost == minimartItemPrice[index], "Low amount");
+        guacToken.transferFrom(msg.sender, nftToTransfer.ownerOf(minimartItemTokenId[index]), minimartItemPrice[index]);
+
+        nftToTransfer.safeTransferFrom(nftToTransfer.ownerOf(minimartItemTokenId[index]), msg.sender, minimartItemTokenId[index]);
+
         delete minimartItemContract[index];
         delete minimartItemTokenId[index];
         delete minimartItemPrice[index];
-        minimartIndex = index;
+
+    }
+
+    function unListItem(uint index) public{
+        nftToTransfer = INft(minimartItemContract[index]);
+
+        require(nftToTransfer.ownerOf(minimartItemTokenId[index]) == msg.sender, "Not the Owner");
+        delete minimartItemContract[index];
+        delete minimartItemTokenId[index];
+        delete minimartItemPrice[index];
+
     }
 
 
